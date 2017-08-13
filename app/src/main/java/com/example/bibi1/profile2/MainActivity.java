57ee9ff46241091351b4a23de75e8057069b803b
Elvira <egazizovai@gmail.com>
2017-08-13 16:07:43 +0300

    added profile view

---
+++ app/src/main/java/com/example/bibi1/profile2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bibi1.profile2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
